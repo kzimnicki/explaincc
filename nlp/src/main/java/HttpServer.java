a7db0c06d1a0381479aaabe856d1dc781f649c71
kz <kz@gmail.com>
2014-11-18 22:34:58 +0100

    Added server

--- nlp/src/main/java/HttpServer.java
+++ nlp/src/main/java/HttpServer.java
@@ -1,26 +1,35 @@
+import com.twitter.finagle.Service;
+import com.twitter.finagle.builder.ServerBuilder;
+import com.twitter.finagle.http.Http;
+import com.twitter.util.Future;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.handler.codec.http.*;
+import org.jboss.netty.util.CharsetUtil;
 
+import java.net.InetSocketAddress;
 
 public class HttpServer {
 
     public static void main(String[] args) {
 
-//        Service<HttpRequest, HttpResponse> service = new Service<HttpRequest, HttpResponse>() {
-//            @Override
-//            public Future<HttpResponse> apply(HttpRequest request) {
-//                System.out.println(request);
-//                HttpResponse res = new DefaultHttpResponse(
-//                        HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
-//                res.setContent(ChannelBuffers.copiedBuffer("Hello World!",
-//                        CharsetUtil.UTF_8));
-//                res.setHeader(HttpHeaders.Names.CONTENT_TYPE,
-//                        "text/plain; charset=UTF-8");
-//                return Future.<HttpResponse> value(res);
-//            }
-//        };
-//
-//        ServerBuilder.safeBuild(service,
-//                ServerBuilder.get().codec(Http.get()).name("HttpServer")
-//                        .bindTo(new InetSocketAddress("localhost", 10000)));
+        NER ner = new NER();
+
+        Service<HttpRequest, HttpResponse> service = new Service<HttpRequest, HttpResponse>() {
+            @Override
+            public Future<HttpResponse> apply(HttpRequest request) {
+                HttpResponse res = new DefaultHttpResponse(
+                        HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+                res.setContent(ChannelBuffers.copiedBuffer("Hello World!",
+                        CharsetUtil.UTF_8));
+                res.setHeader(HttpHeaders.Names.CONTENT_TYPE,
+                        "text/plain; charset=UTF-8");
+                return Future.<HttpResponse> value(res);
+            }
+        };
+
+        ServerBuilder.safeBuild(service,
+                ServerBuilder.get().codec(Http.get()).name("HttpServer")
+                        .bindTo(new InetSocketAddress("localhost", 10000)));
     }
 
 }
--- nlp/src/main/java/NER.java
+++ nlp/src/main/java/NER.java
@@ -1,25 +1,34 @@
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
 import edu.stanford.nlp.ie.AbstractSequenceClassifier;
 import edu.stanford.nlp.ie.crf.CRFClassifier;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 
 import java.util.List;
+import java.util.Map;
 
 public class NER {
 
-    public static void main(String[] args) {
+    public static final AbstractSequenceClassifier CLASSIFIER = CRFClassifier.getClassifierNoExceptions("edu/stanford/nlp/models/ner/english.muc.7class.distsim.crf.ser.gz");
+    public static final Gson GSON = new Gson();
 
-        String serializedClassifier = "edu/stanford/nlp/models/ner/english.muc.7class.distsim.crf.ser.gz";
-        AbstractSequenceClassifier classifier = CRFClassifier.getClassifierNoExceptions(serializedClassifier);
-        String s1 = "Good afternoon Rajat Raina, how are you today? I go to school at Stanford University, which is located in California.";
-        String s2 = "I go to school at Stanford University, which is located in California.";
-        List<List<CoreLabel>> classify = classifier.classify(s1);
+    public String process(String text) {
+        List<List<CoreLabel>> classify = CLASSIFIER.classify(text);
+
+        Map<String, List<String>> map = Maps.newHashMap();
 
         for (List<CoreLabel> words : classify) {
             for (CoreLabel word : words) {
-                System.out.println(word.get(CoreAnnotations.TextAnnotation.class));
-                System.out.println(word.get(CoreAnnotations.AnswerAnnotation.class));
+                String wordValue = String.valueOf(word.get(CoreAnnotations.TextAnnotation.class));
+                String annotation = word.get(CoreAnnotations.AnswerAnnotation.class);
+                if(!map.containsKey(annotation)){
+                    map.put(annotation, Lists.<String>newArrayList());
+                }
+                map.get(annotation).add(wordValue);
             }
         }
+        return GSON.toJson(map);
     }
 }
